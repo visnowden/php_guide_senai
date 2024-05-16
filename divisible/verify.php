a9c32b99b47829ba04514d79499294789f71c607
Visnowden <72585285+visnowden@users.noreply.github.com>
2024-05-16 20:08:21 -0300

    Update verify.php

--- divisible/verify.php
+++ divisible/verify.php
@@ -5,7 +5,7 @@ if (250 % 10 == 0) {
 } elseif (250 % 5 == 0 and 250 % 2 == 0) {
     echo "$value It is divisible only by 5 and 2.";
 } elseif (250 % 5 == 0) {
-    echo "$value It is divisible only by 10, by 5";
+    echo "$value It is divisible only by 10, by 5.";
 } else {
     echo "$value It is divisible only by 2.";
 }
